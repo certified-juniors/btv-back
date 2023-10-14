a6101d251fbbb86c7ac0914a4d7412981f39a985
cry1s <cry1s@ya.ru>
2023-10-14 13:30:20 +0300

    projection

--- app/routes/other.routes.js
+++ app/routes/other.routes.js
@@ -9,9 +9,9 @@ otherRouter.get("/by_region", async (req, res, next) => {
     const nelatitude = +req.query["ne-latitude"];
     const swlongitude = +req.query["sw-longitude"];
     const swlatitude = +req.query["sw-latitude"];
-    const atms_promise = ATMModel.$where(`this.latitude <= ${nelatitude} && this.latitude >= ${swlatitude} && this.longitude <= ${nelongitude} && this.longitude >= ${swlongitude}`);
-    const office_promise = OfficeModel.$where(`this.latitude <= ${nelatitude} && this.latitude >= ${swlatitude} && this.longitude <= ${nelongitude} && this.longitude >= ${swlongitude}`);
-    
+    const atms_promise = ATMModel.find({}, ["latitude", "longitude"]).$where(`this.latitude <= ${nelatitude} && this.latitude >= ${swlatitude} && this.longitude <= ${nelongitude} && this.longitude >= ${swlongitude}`);
+    const office_promise = OfficeModel.find({}, ["latitude", "longitude"]).$where(`this.latitude <= ${nelatitude} && this.latitude >= ${swlatitude} && this.longitude <= ${nelongitude} && this.longitude >= ${swlongitude}`);
+
     res.send({
         atms: await atms_promise,
         offices: await office_promise
