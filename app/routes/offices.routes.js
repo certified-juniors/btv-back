c1d1a58eebb3434976164789b07cda7f0b646bca
cry1s <cry1s@ya.ru>
2023-10-15 02:19:35 +0300

    qrcode

--- app/routes/offices.routes.js
+++ app/routes/offices.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import OfficeModel from "../schemas/offices.schema.js";
-
+import qrcode from "qrcode";
 const officeRouter = Router();
 
 officeRouter.get("/all", (req, res, next) => {
@@ -15,5 +15,21 @@ officeRouter.get('/:id', (req, res, next) => {
         res.send(atm)
     })
 })
+
+officeRouter.post('/take_queue', (req, res) => {
+    const departmentId = req.body._id;
+    if (!departmentId) {
+      return res.status(400).json({ error: 'ID отделения не указан' });
+    }
+    const uniqueText = `${departmentId}_${Date.now()}`;
+  
+    qrcode.toDataURL(uniqueText, (err, qrCode) => {
+      if (err) {
+        return res.status(500).json({ error: 'Ошибка генерации QR-кода' });
+      }
+  
+      res.json({ qrCodeUrl: qrCode });
+    });
+  });
 
 export default officeRouter;
