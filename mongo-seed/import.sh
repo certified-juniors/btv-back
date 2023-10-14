4a6c8dc9eb49dab920f3416af3306766ed95cd1d
cry1s <cry1s@ya.ru>
2023-10-15 01:27:49 +0300

    indexes

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,3 +1,7 @@
-#! /bin/bash
+#!/bin/bash
+
 mongoimport --host mongo -u $MONGO_USERNAME -p $MONGO_PASSWORD --db vtb --collection atms --type json --file /mongo-seed/atms.json --authenticationDatabase=admin --jsonArray
+mongo vtb --host mongo -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase=admin --eval "db.atms.createIndex({longitude: 1, latitude: 1})"
+
 mongoimport --host mongo -u $MONGO_USERNAME -p $MONGO_PASSWORD --db vtb --collection offices --type json --file /mongo-seed/offices.json --authenticationDatabase=admin --jsonArray
+mongo vtb --host mongo -u $MONGO_USERNAME -p $MONGO_PASSWORD --authenticationDatabase=admin --eval "db.offices.createIndex({longitude: 1, latitude: 1})"
