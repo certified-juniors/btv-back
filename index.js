e71865c213465ab9c74ebfb90f7c32fccaee7cae
cry1s <cry1s@ya.ru>
2023-10-14 11:56:41 +0300

    cors?

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { configDotenv } from 'dotenv';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
 import atmsRouter from './app/routes/atms.routes.js';
 import officeRouter from './app/routes/offices.routes.js';
 import otherRouter from './app/routes/other.routes.js';
 import graphhopperRouter from './app/routes/graphhopper.router.js';
+import cors from "cors"
 configDotenv();
 
 const PORT = process.env.EXPRESS_PORT || 5000;
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
+app.use(cors({
+    origin: "*"
+}))
 mongoose.set("strictQuery", false);
 
 const db_url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.DOCKERIZED == 1 ? 'mongo' : 'localhost'}:${process.env.MONGO_PORT}/vtb?directConnection=true&authSource=admin`;
