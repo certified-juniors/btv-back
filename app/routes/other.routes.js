7980b22770f4b37576aa1fbd1b8c6c980f0dc68b
cry1s <cry1s@ya.ru>
2023-10-14 03:31:49 +0300

    get /by_region

---
+++ app/routes/other.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import ATMModel from "../schemas/atms.schema.js";
+import OfficeModel from "../schemas/offices.schema.js";
+
+const otherRouter = Router();
+
+otherRouter.get("/by_region", async (req, res, next) => {
+    const nelongitude = +req.query["ne-longitude"];
+    const nelatitude = +req.query["ne-latitude"];
+    const swlongitude = +req.query["sw-longitude"];
+    const swlatitude = +req.query["sw-latitude"];
+    const atms_promise = ATMModel.$where(`this.latitude <= ${nelatitude} && this.latitude >= ${swlatitude} && this.longitude <= ${nelongitude} && this.longitude >= ${swlongitude}`);
+    const office_promise = OfficeModel.$where(`this.latitude <= ${nelatitude} && this.latitude >= ${swlatitude} && this.longitude <= ${nelongitude} && this.longitude >= ${swlongitude}`);
+    
+    res.send({
+        atms: await atms_promise,
+        offices: await office_promise
+    })
+})
+
+export default otherRouter;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
 import atmsRouter from './app/routes/atms.routes.js';
 import officeRouter from './app/routes/offices.routes.js';
+import otherRouter from './app/routes/other.routes.js';
 configDotenv();
 
 const PORT = process.env.EXPRESS_PORT || 5000;
@@ -13,7 +14,7 @@ const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
+app.use(bodyParser.json())
 mongoose.set("strictQuery", false);
 
 const db_url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.DOCKERIZED == 1 ? 'mongo' : 'localhost'}:${process.env.MONGO_PORT}/vtb?directConnection=true&authSource=admin`;
@@ -22,11 +23,12 @@ async function main() {
     await mongoose.connect(db_url);
     app.use("/atms", atmsRouter)
     app.use("/offices", officeRouter)
+    app.use("/", otherRouter)
     app.listen(PORT, () => {
         console.log("Running on http://localhost:" + PORT + "/");
     });
 }
 
-main().catch(err => console.error(err))
+main()
 
 
